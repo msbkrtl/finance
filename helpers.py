2298753821680ea877236cb1b1889ec8f7b7632d
Musab Kartal <46920010+msbkrtl@users.noreply.github.com>
2020-12-28 22:59:05 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -29,7 +29,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         print(session.get("user_id"))
-        if not session.get('logged_in'):
+        if session.get("user_id") is None:
             return redirect('/login')
         return f(*args, **kwargs)
     return decorated_function
